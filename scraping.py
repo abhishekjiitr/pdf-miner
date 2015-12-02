7b160be8d87b5b6b1d23cf3e2f8483fa2b0593bc
tarunz <tarunk10@hotmail.com>
2015-12-02 16:02:27 +0530

    yoyo

--- scraping.py
+++ scraping.py
@@ -1,28 +1,31 @@
 from bs4 import BeautifulSoup
-import re
+import re, socket
+from urllib.parse import urlparse
 from urllib.parse import urljoin
 import urllib.request, urllib.parse, urllib.error, urllib.request, urllib.error, urllib.parse
 start_url = "http://www.iitb.ac.in/"
 unvisited = []
 unvisited.append(start_url)
+local = socket.gethostbyname(urlparse(start_url).netloc)
 visited = []
 pdfs = []
 def crawl():
     global unvisited
     global visited
+    global local
     website = unvisited.pop()
     visited.append(website)
     #-++779printlkpcc(website)
     req = urllib.request.Request(website)
     response = urllib.request.urlopen(req)
-    print(response.getheader('Content-Type'))
+    print((response.getheader('Content-Type')))
     if  'text/html' in response.getheader('Content-Type'):
         page = response.read()
     else:
         return
     soup = BeautifulSoup(page, 'html.parser')
     #print((soup.prettify()))
-    for link in soup.find_all('a'):
+    for link in soup.find_all('a'):        
         if "http" not in link:
             abs_link = urljoin(website, link.get("href"))
         else:
@@ -33,7 +36,7 @@ def crawl():
                 visited.append(abs_link)
                 pdfs.append(abs_link)
             else:
-                if abs_link not in unvisited:
+                if (abs_link not in unvisited) and (socket.gethostbyname(urlparse(abs_link).netloc) == local):
                     unvisited.append(abs_link)
 
 

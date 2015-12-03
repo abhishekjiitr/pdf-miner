6d4d193bd809e6c71c1f89475bd6e9f38dd7aba3
Abhishek Jaisingh <abhi2254015@gmail.com>
2015-12-03 18:04:44 +0530

    Store temp

--- list_store.py
+++ list_store.py
@@ -1,4 +1,18 @@
+from urllib.parse import urlparse
 import pickle
+import os
+from time import time
+
+
+time = time()
+hostname = urlparse("http://www.iitr.ac.in/hi/departments/DPT/pages/Research+Publications.html").netloc
+abs_path = hostname+"/"+str(time)+".p"
+if not os.path.exists(hostname):
+    os.makedirs(hostname)
+doc = open(abs_path, "wb")
+print(hostname)
 l = [1, 2, 3]
-doc = open("pickle", "w") 
 pickle.dump(l, doc)
+doc.close()
+l = pickle.load(open(abs_path, "rb"))
+print(l)
